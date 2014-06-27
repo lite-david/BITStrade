2e1d08f4687816ca5521322159539c8d637d3622
lite-david <edwin.mascarenhas95@gmail.com>
2014-06-27 16:13:26 +0530

    added a validate function

--- login.php
+++ login.php
@@ -3,18 +3,30 @@
 session_start();
 if(isset($_POST['login']))
 {
-	$nick=mysql_real_escape_string($_POST['nick']);
-	$password=mysql_real_escape_string($_POST['password']);
+	function validate($a)
+	{
+		$a=trim($a);
+		$a=strip_tags($a);
+		$a=mysql_real_escape_string($a);
+		return $a;
+	}
+
+
+	$nick=validate($_POST['nick']);
+	$password=validate($_POST['password']);
 	
 	if($nick && $password)
 	{
 		$connect= mysql_connect("127.0.0.1","root","") or die("cannot connect");
 		mysql_select_db("bitstrade");
 		$login=mysql_query("SELECT * FROM users WHERE nick='$nick'");
+	
+		
 		while($log=mysql_fetch_assoc($login))
 		{
 			$dbnick = $log['nick'];
 			$dbpassword = $log['password'];
+		
 		}
 		
 		if( $nick==$dbnick && $password==$dbpassword )
@@ -27,6 +39,7 @@ if(isset($_POST['login']))
 		else
 		{
 			header("location:http://localhost/BITSsell.php? notify= incorrect nick and password combination");
+				
 		}
 		
 
