ba420956ba7fca6c56652574e84e2e13b2fa11ed
lite-david <edwin.mascarenhas95@gmail.com>
2014-06-27 16:01:13 +0530

    added a validate() function

--- del.php
+++ del.php
@@ -2,9 +2,17 @@
 
 session_start();
 
+function validate($a)
+	{
+		$a=trim($a);
+		$a=strip_tags($a);
+		$a=mysql_real_escape_string($a);
+		return $a;
+	}
+
 if(isset($_POST['delete']))
 {
-	$product=$_POST['product'];
+	$product=validate($_POST['product']);
 	mysql_connect("127.0.0.1","root","") or die("could not connect");
 	mysql_select_db("bitstrade");
 	mysql_query("DELETE from sell WHERE product='$product'");
