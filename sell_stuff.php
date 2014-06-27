9bd6d43620e1c982eba53c93ea749d07af071a92
lite-david <edwin.mascarenhas95@gmail.com>
2014-06-27 16:18:01 +0530

    added a validate function

--- sell_stuff.php
+++ sell_stuff.php
@@ -4,19 +4,27 @@ session_start();
 
 if(isset($_POST['submit']))
 {
-	$name=mysql_real_escape_string($_POST['name']);
-	$hostel=mysql_real_escape_string($_POST['hostel']);
-	$product=mysql_real_escape_string($_POST['product']);
-	$details=mysql_real_escape_string($_POST['details']);
-	$price=mysql_real_escape_string($_POST['price']);
-	$number=mysql_real_escape_string($_POST['number']);
+	function validate($a)
+	{
+		$a=trim($a);
+		$a=strip_tags($a);
+		$a=mysql_real_escape_string($a);
+		return $a;
+	}
+
+	$name=validate($_POST['name']);
+	$hostel=validate($_POST['hostel']);
+	$product=validate($_POST['product']);
+	$details=validate($_POST['details']);
+	$price=validate($_POST['price']);
+	$number=validate($_POST['number']);
 	$nick=$_SESSION['nick'];
+	
 	if( $hostel && $product && $number)
 	{
 		mysql_connect("127.0.0.1","root","") or die("could not connect");
 		mysql_select_db("bitstrade");
 		mysql_query("INSERT INTO sell(nick,name,hostel,product,details,price,contact)VALUES('$nick','$name','$hostel','$product','$details','$price','$number')");
-		//echo "<B> congrats data addded successfully</B>";
 		header("location:members.php?notify=Success");
 	
 		
