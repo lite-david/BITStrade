008e6697c32cc3fa0bdd71a3bb29d77690f6dd7f
lite-david <edwin.mascarenhas95@gmail.com>
2014-07-18 14:00:29 +0530

    search products

---
+++ search.php
@@ -0,0 +1,58 @@
+<?php
+
+session_start();
+$output='';	
+if(isset($_GET['submit']))
+{
+	function validate($a)
+	{
+		$a=trim($a);
+		$a=strip_tags($a);
+		$a=mysql_real_escape_string($a);
+		return $a;
+	}
+	
+	$search=validate($_GET['search']);
+	
+	if($search)
+	{
+		mysql_connect("127.0.0.1","root","") or die("Could not connect");
+		mysql_select_db("bitstrade");
+		$search= preg_replace("#[^0-9a-z]#i","","$search");
+		$query= mysql_query("SELECT * FROM sell WHERE nick LIKE '%$search%' OR product LIKE '%$search%'");
+		$count= mysql_num_rows($query);
+		
+		if($count == 0)
+		{
+			echo "sorry nothing matched your search term!";
+		}
+		
+		else
+		{
+			while($l=mysql_fetch_array($query))
+			{
+				$nick=htmlspecialchars($l['nick']);
+				$product=htmlspecialchars($l['product']);
+				$hostel=htmlspecialchars($l['hostel']);
+				$details=htmlspecialchars($l['details']);
+				$number=htmlspecialchars($l['contact']);
+				$name=htmlspecialchars($l['name']);
+				$price=htmlspecialchars($l['price']);
+				
+				
+				$output.='<div>nick: '.$nick.'<br> product: '.$product.'<br>details: '.$details.'<br>price: '.$price.'<br> contact: '.$number.'<br>hostel: '.$hostel.'</div><br><br>';
+			}
+		}
+	}	
+		
+	else
+	{
+		header("location:index.php?notify=Enter a valid search term");
+	}
+}
+?>
+<?php
+
+print("$output"); 
+?>
+
